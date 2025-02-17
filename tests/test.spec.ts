12a0204746326c15314d990a9f91c29ecbe328d8
Mikkel Agerlin <ma@complycloud.com>
2025-02-17 19:31:54 +0100

    feat: recency test

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -74,6 +74,51 @@ test("sortedByFrecency prefers multiple usages", () => {
   updateFrecencyData(query, multipleUsageMatchingId, key);
   updateFrecencyData(query, multipleUsageMatchingId, key);
 
+  // Assert
   const updatedSorted = sortByFrecency(query, items, key);
   expect(updatedSorted[0].matchId).toBe(multipleUsageMatchingId);
+});
+
+test("sortedByFrecency prefers recent usages", () => {
+  // Arrange
+  const key = "testing-key";
+  const query = "on";
+  const matchingId = "1";
+  const moreRecentUsageMatchingId = "2";
+  const items = [
+    { matchId: "0", label: "zero" },
+    { matchId: matchingId, label: "one" },
+    { matchId: moreRecentUsageMatchingId, label: "two" },
+    { matchId: "3", label: "three" },
+  ];
+
+  const localStorageHistory = [
+    [
+      query,
+      {
+        selectedMatches: [
+          {
+            matchId: matchingId,
+            visits: [
+              "Mon Jan 01 2025 19:11:37 GMT+0100 (Central European Standard Time)",
+              "Mon Jan 02 2025 19:11:37 GMT+0100 (Central European Standard Time)",
+              "Mon Jan 03 2025 19:11:37 GMT+0100 (Central European Standard Time)",
+            ],
+          },
+
+          {
+            matchId: moreRecentUsageMatchingId,
+            visits: [
+              "Mon Feb 17 2025 19:11:37 GMT+0100 (Central European Standard Time)",
+            ],
+          },
+        ],
+      },
+    ],
+  ];
+  localStorage.setItem(key, JSON.stringify(localStorageHistory));
+
+  // Act
+  const sorted = sortByFrecency(query, items, key);
+  expect(sorted[0].matchId).toBe(moreRecentUsageMatchingId);
 });
