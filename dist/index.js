83483ad8f5e4598708c2e32a20bbf1e2ac7574e0
Mikkel Agerlin <ma@complycloud.com>
2025-02-17 18:59:38 +0100

    fix: no index file

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var frecency_sort_1 = require("./frecency-sort");
-module.exports = {
-    sortByFrecency: frecency_sort_1.sortByFrecency,
-    updateFrecencyData: frecency_sort_1.updateFrecencyData,
-};
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { sortByFrecency, updateFrecencyData } from "./frecency-sort";
-
-module.exports = {
-  sortByFrecency,
-  updateFrecencyData,
-};
