4f0b1763a9d3fbf1955c4e4008ebeb4f88c6d96e
Mikkel Agerlin <ma@complycloud.com>
2025-02-17 19:17:16 +0100

    fix: cleanup dist in git

--- dist/frecency-sort.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const compareByFrecency: (needle: string, matchAId: string, matchBId: string, localStorageKey: string) => number;
-export declare function sortByFrecency<T extends {
-    matchId: string;
-}>(query: string, listItems: T[], key: string): T[];
-export declare const updateFrecencyData: (query: string, selectedMatchId: string, localStorageKey: string) => void;
--- dist/types.d.ts
+++
@@ -1,8 +0,0 @@
-export type StoredQueryMatch = {
-    matchId: string;
-    visits: string[];
-};
-export type StoredQuery = {
-    selectedMatches: StoredQueryMatch[];
-};
-export type StoredQueryMap = Map<string, StoredQuery>;
